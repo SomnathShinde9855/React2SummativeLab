d556de6c2286a5b82c69996f16115873d701c7ca
Somnath Shinde <somnathshinde9855@gmail.com>
2026-07-11 17:59:39 +0530

    React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar'
 import { StoreProvider } from './context/StoreContext'
@@ -6,12 +7,28 @@ import AddProduct from './pages/AddProduct'
 import Home from './pages/Home'
 import Products from './pages/Products'
 
+function RedirectHandler() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    const redirectPath = params.get('redirect')
+
+    if (redirectPath) {
+      navigate(redirectPath.replace('/React2SummativeLab', '') || '/', { replace: true })
+    }
+  }, [navigate])
+
+  return null
+}
+
 function App() {
   const routerBasename = import.meta.env.PROD ? '/React2SummativeLab' : '/'
 
   return (
     <StoreProvider>
       <BrowserRouter basename={routerBasename}>
+        <RedirectHandler />
         <div className="app-shell">
           <NavBar />
           <main className="page-content">
